729706f66e14647deba3c9ac59c224b974c94163
rupalidash <rupalidash@localhost>
2021-11-20 22:08:06 -0800

    First Draft

---
+++ TelaDocTest/src/main/java/com/teladoc/qa/pages/HomeAddPage.java
@@ -0,0 +1,131 @@
+package com.teladoc.qa.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import com.teladoc.qa.base.TestBase;
+
+public class HomeAddPage extends TestBase{
+	
+	
+	@FindBy(xpath="//button[@class='btn ng-scope ng-binding btn-primary']")
+	WebElement okButton;
+	
+	
+	@FindBy(css="tbody>tr>td:nth-of-type(3)")
+	List<WebElement> list;
+	
+	@FindBy(css="button[type='add']")
+	WebElement addUser;
+	
+	@FindBy(css="input[name='FirstName']")
+	WebElement firstName;
+	
+	@FindBy(css="input[name='LastName']")
+	WebElement lastName;
+	
+	@FindBy(css="input[name='UserName']")
+	WebElement userName;
+	
+	@FindBy(css="input[name='Password']")
+	WebElement passWord;
+	
+	@FindBy(css="input[value='16']")
+	WebElement customer;
+	
+	@FindBy(css="select[name='RoleId']")
+	WebElement roleID;
+	
+	
+	@FindBy(css="input[name='Email']")
+	WebElement email;
+	
+	@FindBy(css="input[name='Mobilephone']")
+	WebElement cellPhone;
+	
+	@FindBy(css="button[ng-click='save(user)']")
+	WebElement save;
+	
+	
+	
+	public void addUser(String firstname, String lastname, String username, String password, String role, String emailaddress, String phonenumber) {
+		addUser.click();
+		firstName.sendKeys(firstname);
+		lastName.sendKeys(lastname);
+		userName.sendKeys(username);
+		passWord.sendKeys(password);
+		customer.click();
+		Select roles=new Select(roleID);
+		roles.selectByVisibleText(role);
+		email.sendKeys(emailaddress);
+		cellPhone.sendKeys(phonenumber);		
+		save.click();
+		
+	}
+	public HomeAddPage() {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	public String validateUser() {
+		
+		
+		String actualUser="";
+		for(WebElement ele : list) {
+			
+			if (ele.getText().equalsIgnoreCase(prop.getProperty("userAdded"))) {
+				actualUser=ele.getText();
+				
+				
+			}
+		}
+		
+		return actualUser;
+	}
+	
+	public void deleteUser() {
+		
+		
+		list=driver.findElements(By.cssSelector("tbody>tr>td:nth-of-type(3)"));
+		for (int i=0;i<list.size();i++) {
+			if (list.get(i).getText().equalsIgnoreCase(prop.getProperty("userDeleted"))) {
+				
+				int pos=i+1;
+				
+		    	 WebElement del=driver.findElement(By.cssSelector("tbody>tr:nth-of-type("+pos+")>td:nth-of-type(11)>button"));
+		    	
+		    	 del.click();
+		    	 okButton.click();
+		    	 break;
+		      }
+			
+		}
+
+		
+	}
+	public boolean checkIfUserDeleted() {
+		list=driver.findElements(By.cssSelector("tbody>tr>td:nth-of-type(3)"));
+		int count=0;
+		for(int i=0;i<list.size();i++) {
+			if(list.get(i).getText().equals(prop.getProperty("userDeleted"))) {
+				break;
+			}
+			else {
+				count++;
+			}
+		}
+		if(count == list.size()) {
+			return true;
+		}else {
+			return false;
+		}		
+	}
+	
+}
---
+++ TelaDocTest/src/test/java/com/teladoc/qa/testcases/UserTest.java
@@ -0,0 +1,60 @@
+package com.teladoc.qa.testcases;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.teladoc.qa.util.TestUtil;
+import com.teladoc.qa.base.TestBase;
+import com.teladoc.qa.pages.HomeAddPage;
+
+public class UserTest extends TestBase{
+	HomeAddPage homeAddPage;
+	TestUtil testUtil;
+	String sheetName = "UserDetails";
+	
+	@BeforeClass
+	public void setUp() {
+		initialization();
+		homeAddPage=new HomeAddPage();
+		testUtil = new TestUtil();
+	}
+	
+	@DataProvider
+	public Object[][] getTestData() throws InvalidFormatException{
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}
+	
+	@Test(priority=1, dataProvider="getTestData")  // Add User Test
+	public void addUserTest(String firstname, String lastname, String username, String password, String role, String emailaddress, String phonenumber) {
+		homeAddPage.addUser(firstname,lastname,username,password, role, emailaddress, phonenumber);
+	}
+	
+	@Test(priority=2)  // Verify the User added 
+	public void verifyAddedUserTest() {
+		
+		
+		String actualUser=homeAddPage.validateUser();
+		Assert.assertEquals(actualUser, prop.getProperty("userAdded"));	
+	}
+	
+	@Test(priority=3)  // Delete the User and verify if its removed from the list 
+	public void verifyDeleteUserTest() {
+		
+	
+		homeAddPage.deleteUser();
+		Assert.assertTrue(homeAddPage.checkIfUserDeleted());	
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+	
+}

56e162641f56e5dd9f4f142205dbf49e9e73553d
rupalidash <rupalidash@localhost>
2021-11-21 21:09:59 -0800

    Removed the extra lines of code

--- TelaDocTest/src/main/java/com/teladoc/qa/util/TestUtil.java
+++ TelaDocTest/src/main/java/com/teladoc/qa/util/TestUtil.java
@@ -20,12 +20,10 @@ public class TestUtil extends TestBase {
 	
 		public static long PAGE_LOAD_TIMEOUT = 20;
 		public static long IMPLICIT_WAIT = 20;
-		
 		public static String TESTDATA_SHEET_PATH = "/src/main/java/com/teladoc/qa/testdata/ExcelTestData.xlsx";
-
-		static Workbook book;
-		static Sheet sheet;
-		static JavascriptExecutor js;
+		public static Workbook book;
+		public static Sheet sheet;
+		
 
 		public static Object[][] getTestData(String sheetName) throws InvalidFormatException {
 			FileInputStream file = null;
@@ -48,9 +46,5 @@ public class TestUtil extends TestBase {
 			}
 			return data;
 		}		
-		public static void takeScreenshotAtEndOfTest() throws IOException {
-			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-			String currentDir = System.getProperty("user.dir");
-			FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
-		}
+		
 }
